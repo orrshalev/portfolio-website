7df519c37680998e0a2f3f7af11a10eddea51e49
Orr Shalev <shalev.orr@gmail.com>
2023-01-02 15:38:15 -0500

    Before pc import

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -1,71 +1,89 @@
 import React from "react";
+import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
 import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 
-type LinkProps = {
+type PageLinkProps = {
   page: string;
   url: string;
   iconName: string;
   marginLeft: string;
   isTopOfPage: boolean;
+  isExternal?: boolean;
+  dimensions: number;
 };
 
-const Link = (props: LinkProps) => {
-  const { isTopOfPage, page, url, iconName, marginLeft } = props;
+const PageLink = (props: PageLinkProps) => {
+  const {
+    dimensions,
+    isExternal,
+    isTopOfPage,
+    page,
+    url,
+    iconName,
+    marginLeft,
+  } = props;
   const [isHovered, setIsHovered] = useState(false);
+  const animatedButton = (
+    <button
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+    >
+      <motion.span
+        className="opacity-100"
+        transition={{ duration: 0.2 }}
+        animate={{ opacity: isHovered ? 0.0 : 1.0 }}
+      >
+        {isTopOfPage ? (
+          <Image
+            className={`absolute -top-1 ${marginLeft} z-10 opacity-100`}
+            src={`/assets/svg/${iconName}-black.svg`}
+            alt="file"
+            width={dimensions}
+            height={dimensions}
+          />
+        ) : (
+          <>
+            <Image
+              className={`absolute -top-1 ${marginLeft} z-10 opacity-100`}
+              src={`/assets/svg/${iconName}-yellow.svg`}
+              alt="file"
+              width={dimensions}
+              height={dimensions}
+            />
+          </>
+        )}
+      </motion.span>
+      <motion.span
+        className={`z-20 opacity-0`}
+        transition={{ duration: 0.2 }}
+        animate={{ opacity: isHovered ? 1.0 : 0.0 }}
+      >
+        <span className={`${isTopOfPage ? "text-black" : "text-yellow"}`}>
+          {page}
+        </span>
+      </motion.span>
+    </button>
+  );
 
   return (
     <motion.div
       animate={{ rotate: isHovered ? 360 : 0 }}
       transition={{ duration: 0.25 }}
     >
-      <a href={url} target="_blank" rel="noreferrer">
-        <button
-          onMouseEnter={() => {
-            setIsHovered(true);
-          }}
-          onMouseLeave={() => {
-            setIsHovered(false);
-          }}
-        >
-          <motion.span
-            className="opacity-100"
-            transition={{ duration: 0.2 }}
-            animate={{ opacity: isHovered ? 0.0 : 1.0 }}
-          >
-            {isTopOfPage ? (
-              <Image
-                className={`absolute -top-1 ${marginLeft} z-10 opacity-100`}
-                src={`/assets/svg/${iconName}-black.svg`}
-                alt="file"
-                width={20}
-                height={20}
-              />
-            ) : (
-              <>
-                <Image
-                  className={`absolute -top-1 ${marginLeft} z-10 opacity-100`}
-                  src={`/assets/svg/${iconName}-yellow.svg`}
-                  alt="file"
-                  width={20}
-                  height={20}
-                />
-              </>
-            )}
-          </motion.span>
-          <motion.span
-            className={`z-20 opacity-0`}
-            transition={{ duration: 0.2 }}
-            animate={{ opacity: isHovered ? 1.0 : 0.0 }}
-          >
-            <span className={`${isTopOfPage ? "text-black" : "text-yellow"}`}>
-              {page}
-            </span>
-          </motion.span>
-        </button>
-      </a>
+      {isExternal ? (
+        <a href={url} target="_blank" rel="noreferrer">
+          {animatedButton}
+        </a>
+      ) : (
+        <Link href={url}>{animatedButton}</Link>
+      )}
     </motion.div>
   );
 };
@@ -98,12 +116,30 @@ const Navbar = (props: NavbarProps) => {
           <div
             className={`relative flex h-full justify-between gap-16 font-opensans text-sm font-semibold`}
           >
-            <Link
+            <PageLink
+              page="Home"
+              url="/"
+              marginLeft="ml-1"
+              iconName="house-solid"
+              isTopOfPage={isTopOfPage}
+              dimensions={30}
+            />
+            <PageLink
               page="Resume"
               url="/General-Resume.pdf"
               marginLeft="ml-4"
               iconName="file-solid"
               isTopOfPage={isTopOfPage}
+              isExternal={true}
+              dimensions={20}
+            />
+            <PageLink
+              page="About"
+              url="/about"
+              marginLeft="ml-2"
+              iconName="clipboard-question-solid"
+              isTopOfPage={isTopOfPage}
+              dimensions={22}
             />
           </div>
         ) : (
